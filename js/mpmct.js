03e2fa9c1820b409cda02a4269fb29f735ba2123
dojoarun <31395594+dojoarun@users.noreply.github.com>
2020-10-21 13:56:08 +0530

    Add files via upload

---
+++ js/mpmct.js
@@ -0,0 +1,19 @@
+function showSubmenu() {
+    document.getElementById('subMenu').style.display = 'block';
+}
+
+function hideSubmenu() {
+    document.getElementById('subMenu').style.display = 'none';
+}
+
+function redirectHome() {
+	window.location="index.html";
+}
+
+function showElement(elementId) {
+    document.getElementById(elementId).style.display = 'block';
+}
+
+function hideElement(elementId) {
+	document.getElementById(elementId).style.display = 'none';
+}
